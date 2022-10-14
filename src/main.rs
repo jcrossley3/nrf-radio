a0cf3dc501a9ad75e2af6eb2feb08928c1404583
Jim Crossley <jim@crossleys.org>
2022-10-14 13:10:09 -0400

    Adapt for embed/rtt/nrf52840dk

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@
 
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+                     // use panic_abort as _; // requires nightly
+                     // use panic_itm as _; // logs messages over ITM; requires ITM support
+                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+
+    rprintln!("hello");
+
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
 
     loop {
